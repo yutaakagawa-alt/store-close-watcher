cb4f2908cc7fd12d0b547c7660d8f66a9159f8c6
yutaakagawa-alt <yutaakagawa@gmail.com>
2026-01-05 00:43:25 +0900

    Update send_test.py

--- send_test.py
+++ send_test.py
@@ -1,23 +1,6 @@
-import requests
 import os
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
-USER_ID = os.environ.get("LINE_USER_ID")
+token = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
 
-url = "https://api.line.me/v2/bot/message/broadcast"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
-}
-
-data = {
-    "messages": [
-        {
-            "type": "text",
-            "text": "✅ LINE通知テスト成功です"
-        }
-    ]
-}
-
-response = requests.post(url, headers=headers, json=data)
-print(response.status_code, response.text)
+print("TOKEN EXISTS:", token is not None)
+print("TOKEN LENGTH:", len(token) if token else 0)
