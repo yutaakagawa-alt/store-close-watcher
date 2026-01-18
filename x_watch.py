793ec6e608acf2ff381b012302009b1c911963ce
yutaakagawa-alt <yutaakagawa@gmail.com>
2026-01-18 15:23:49 +0900

    Update x_watch.py

--- x_watch.py
+++ x_watch.py
@@ -15,7 +15,10 @@ query = "TSUTAYA 閉店"
 url = f"https://x.com/search?q={query}&f=live"
 
 driver.get(url)
-time.sleep(5)  # 読み込み待ち
+time.sleep(10)  # 読み込み待ち
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(5)
 
 links = set()
 elements = driver.find_elements(By.XPATH, "//a[contains(@href,'/status/')]")
@@ -24,6 +27,8 @@ for e in elements:
     links.add(e.get_attribute("href"))
 
 driver.quit()
+
+print("PAGE TITLE:", driver.title)
 
 print("X results:")
 for link in list(links)[:10]:
